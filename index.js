9bc9c517acc9866cb757a60b919ca080a135f9a6
dengula <cocanphaizkko@gmail.com>
2024-08-02 15:45:25 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 // Import packages
 const express = require("express");
 const home = require("./routes/home");
-
+const path = require("./home");
 // Middlewares
 const app = express();
 app.use(express.json());
 // Routes
 app.use("/add", home);
-
+app.use(express.static(path.join(__dirname, 'public')));
 // connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
