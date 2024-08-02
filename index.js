04b6b5d438b9c8d3ec6e9afc7383ba4c5e8bc813
dengula <cocanphaizkko@gmail.com>
2024-08-02 15:48:32 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
-// Import packages
 const express = require("express");
+const path = require("path"); // Để xử lý đường dẫn đến thư mục và tệp
 const home = require("./routes/home");
-const path = require("./home");
+
 // Middlewares
 const app = express();
 app.use(express.json());
-// Routes
-app.use("/add", home);
+
+// Cấu hình để phục vụ các tệp tĩnh từ thư mục 'public'
 app.use(express.static(path.join(__dirname, 'public')));
-// connection
+
+// Routes
+app.use("/home", home);
+
+// Connection
 const port = process.env.PORT || 9001;
 app.listen(port, () => console.log(`Listening to port ${port}`));
